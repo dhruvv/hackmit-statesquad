06e706ce9b72910bc46e000973548d092fb7b2af
Dhruv Venkataraman <dhruv@dhruvv.tech>
2022-10-01 17:53:27 -0400

    added most of the FE

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,10 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import TextField from '@mui/material/TextField';
+import SearchIcon from '@mui/icons-material/Search';
 
 function App() {
   return (
@@ -27,6 +31,27 @@ function App() {
         <Button color="inherit">Login</Button>
       </Toolbar>
     </AppBar>
+    <Card>
+      <CardContent>
+      <Typography variant="body2" color="text.secondary">
+         <TextField fullWidth label="Enter URL here" id="youtubeID" />
+         <IconButton aria-label="delete" style={{
+    display: 'flex',
+    alignItems: 'center',
+    flexWrap: 'wrap',
+}}>
+            <SearchIcon  />
+          </IconButton>
+      </Typography>
+      </CardContent>
+    </Card>
+    <Card>
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          Please enter a URL!
+          </Typography>
+      </CardContent>
+    </Card>
   </Box>
   );
 }
